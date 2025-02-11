4812c5bbd29a46dfaf5a195ce00bb87b3855968a
Mahdi Sanatkar <m.sanatkar@ipaam.ir>
2025-02-11 20:13:33 +0330

    fix: remove welcome

--- apps/sandbox/src/app/app.routes.ts
+++ apps/sandbox/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('@hela/survey-ui/features').then((c) => c.ModelerComponent),
+  },
+];
