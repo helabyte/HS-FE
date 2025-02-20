c5f7540ddbf6e9d921e494016bd786d2d3738c4e
Mahdi Sanatkar <m.sanatkar@ipaam.ir>
2025-02-20 09:58:47 +0330

    feat: wip

--- packages/survey-ui/components/src/my-account/my-account.component.spec.ts
+++ packages/survey-ui/components/src/my-account/my-account.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { MyAccountComponent } from './my-account.component';
 
 describe('MyAccountComponent', () => {
--- packages/survey-ui/features/src/my-account-page/my-account-page.component.spec.ts
+++ packages/survey-ui/features/src/my-account-page/my-account-page.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { MyAccountPageComponent } from './my-account-page.component';
 
 describe('MyAccountPageComponent', () => {
