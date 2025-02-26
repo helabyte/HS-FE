904c9c6e25d7b5f46c630ad55765eb970e49e65b
psychomet <psychomet.dev@gmail.com>
2025-02-26 09:57:28 +0330

    fix(sandbox): move env currect folder

--- apps/sandbox/src/main.ts
+++ apps/sandbox/src/main.ts
@@ -2,9 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { appConfig } from '@hela/survey-ui/features';
 
-import { environment } from '../environments/environment';
-
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, appConfig(environment)).catch((err) =>
   console.error(err)
